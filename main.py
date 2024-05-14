38b9b72d6d940838c856d9c4e4678801de2d5b9e
DaniloCossioloDias <116849332+DaniloCossioloDias@users.noreply.github.com>
2024-05-14 12:34:51 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from PIL import Image
-im = Image.open("./img/")
+import hashlib
+
+def calcular_hash_imagem(imagem, algoritmo='sha256'):
+    with open(imagem, 'rb') as arquivo_imagem:
+        conteudo = arquivo_imagem.read()
+        hash_imagem = hashlib.new(algoritmo, conteudo).hexdigest()
+    return hash_imagem
+
+imagem = "./img/IMG_20240322_204840.jpg"
+
+im = Image.open(imagem)
 
 exif_data = im._getexif()
-print(exif_data)
 
-for key, value in exif_data.items():
-    print(key, value)
+dados = {
+    0x9003: "Data e hora", 
+    0x010F: "Fabricante",   
+    0x0110: "Modelo"   
+}
+
+for chave, value in exif_data.items():
+    if chave in dados:
+        print(dados[chave] + ":", value)
+
+hash_imagem = calcular_hash_imagem(imagem)
+
+print("Hash da imagem:", hash_imagem)
